dbac70678b228da0db2015ba6b5f4e95f471d9ae
cvarun <cvarun561@gmail.com>
2023-09-13 02:14:28 +0530

    Added Govt.Schemes screen and multilingual words

---
+++ app/src/main/java/com/sih/graminshikshasahyog/adapters/GovtSchemesAdapter.kt
@@ -0,0 +1,33 @@
+package com.sih.graminshikshasahyog.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.sih.graminshikshasahyog.databinding.GovtSchemeItemBinding
+import com.sih.graminshikshasahyog.model.GovtSchemes
+
+class GovtSchemesAdapter (
+
+    val schemeList:List<GovtSchemes>
+    ): RecyclerView.Adapter<GovtSchemesAdapter.SchemeViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SchemeViewHolder {
+        val binding=GovtSchemeItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return SchemeViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: GovtSchemesAdapter.SchemeViewHolder, position: Int) {
+        val schemeItem:GovtSchemes=schemeList[position]
+        holder.bind(schemeItem)
+    }
+
+    override fun getItemCount()= schemeList.size
+
+    class SchemeViewHolder(private val binding:GovtSchemeItemBinding):RecyclerView.ViewHolder(binding.root){
+
+        fun bind(schemeItem:GovtSchemes){
+            binding.tvSchemeTitle.text=schemeItem.scheme
+            binding.tvEligibility.text=schemeItem.eligibility
+    }
+    }
+    }

98653a63cddd3f68434ea145f8118ac9be7e25b3
Ayush Keshri <keshri.ayush019@gmail.com>
2023-09-16 01:46:05 +0530

    implemented sign in with forgot password feature

--- app/src/main/java/com/sih/graminshikshasahyog/ui/activities/SignInActivity.kt
+++ app/src/main/java/com/sih/graminshikshasahyog/ui/activities/SignInActivity.kt
@@ -1,14 +1,19 @@
 package com.sih.graminshikshasahyog.ui.activities
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
 import com.google.firebase.auth.FirebaseAuth
 import com.sih.graminshikshasahyog.R
+import com.sih.graminshikshasahyog.adapters.ViewPagerAdapter1
+import com.sih.graminshikshasahyog.adapters.ViewPagerAdapter5
 import com.sih.graminshikshasahyog.databinding.ActivitySignInBinding
 
 class SignInActivity : AppCompatActivity() {
@@ -21,54 +26,49 @@ class SignInActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         auth = FirebaseAuth.getInstance()
-        binding.tvNewHere.setOnClickListener {
-            newHere()
-        }
 
-        binding.btnSignIn.setOnClickListener {
-            signIn()
-        }
+        val tabLayout = binding.tabLayout
+        val viewPager = binding.viewPager
 
-        binding.tvForgotPassword.setOnClickListener {
-            forgotPassword()
-        }
+        val adapter = ViewPagerAdapter5(supportFragmentManager, lifecycle)
+        createTabs(tabLayout, getString(R.string.as_a_student))
+        createTabs(tabLayout, getString(R.string.as_a_mentor))
+        viewPager.adapter = adapter
 
-    }
+        tabLayout.addOnTabSelectedListener(
+            object: TabLayout.OnTabSelectedListener {
+                override fun onTabSelected(tab: TabLayout.Tab?) {
+                    if(tab != null) {
+                        viewPager.currentItem = tab.position
+                        if(tab.text == "Mentor") {
+                            tabLayout.setSelectedTabIndicatorColor(Color.parseColor("#0CB407"))
+                            tabLayout.setSelectedTabIndicatorHeight((5 * resources.displayMetrics.density).toInt())
+                            tabLayout.setTabTextColors(Color.parseColor("#FF000000"), Color.parseColor("#0CB407"))
+                        } else if(tab.text == "Student"){
+                            tabLayout.setSelectedTabIndicatorColor(Color.parseColor("#ED8129"))
+                            tabLayout.setSelectedTabIndicatorHeight((5 * resources.displayMetrics.density).toInt())
+                            tabLayout.setTabTextColors(Color.parseColor("#FF000000"), Color.parseColor("#ED8129"))
+                        }
 
-    private fun forgotPassword() {
+                    }
+                }
 
-    }
+                override fun onTabUnselected(tab: TabLayout.Tab?) {
+                }
 
-    private fun newHere() {
-        val intent = Intent(this, StudentSignUpActivity::class.java)
-        startActivity(intent)
-        finish()
-    }
-
-    private fun signIn() {
-        val email = binding.etSIEmail.text.toString().trim()
-        val pass = binding.etSIPass.text.toString().trim()
-        auth = FirebaseAuth.getInstance()
-        if (email.isEmpty() == true or pass.isEmpty()) {
-            Toast.makeText(this@SignInActivity, "Enter email and password!", Toast.LENGTH_SHORT)
-                .show()
-        } else {
-            auth.signInWithEmailAndPassword(email, pass)
-                .addOnCompleteListener {
-                if (it.isSuccessful) {
-                    val intent = Intent(this, MainActivity::class.java)
-                    Toast.makeText(this, "Logged In", Toast.LENGTH_SHORT).show()
-                    startActivity(intent)
-                    finish()
-                } else {
-                    Toast.makeText(
-                        this@SignInActivity,
-                        "Credentials do not match",
-                        Toast.LENGTH_SHORT
-                    ).show()
+                override fun onTabReselected(tab: TabLayout.Tab?) {
                 }
             }
-        }
+        )
 
+        viewPager.registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                tabLayout.selectTab(tabLayout.getTabAt(position))
+            }
+        })
+    }
+    private fun createTabs(tabLayout: TabLayout, text: String) {
+        tabLayout.addTab(tabLayout.newTab().setText(text))
     }
 }
